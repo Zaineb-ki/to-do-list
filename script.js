39f5ca6179783256aa358c9781546142dcac8278
Zaineb-ki <kossorizaineb@gmail.com>
2020-03-03 15:17:25 +0100

    Last modification du code js

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 var Conteur = 1;
 function add(){
     var text = document.getElementById('text').value;
-    var content = document.getElementById('content'); 
-    content.innerHTML += '<div><input type="checkbox" name="" onclick="check(this)" id="'+Conteur+'"><label id="A'+Conteur+'">'+text+'</label><br></div>';
+	var content = document.getElementById('content'); 
+	
+
+	var taskDiv = document.createElement('DIV')
+	var task = document.createElement("INPUT")
+	var taskLabel = document.createElement("LABEL")
+	
+	task.setAttribute("type", "checkbox")
+	task.setAttribute("onclick", "check(this)")
+	task.setAttribute("id", "task"+Conteur)
+	taskLabel.innerHTML = text
+	taskLabel.setAttribute("id", "Atask"+Conteur)
+
+	taskDiv.appendChild(task)
+	taskDiv.appendChild(taskLabel)
+	content.appendChild(taskDiv)
+//    content.innerHTML += '<input type="checkbox" onclick="check(this)" id="task'+Conteur+'"><label id="Atask'+Conteur+'">'+text+'</label><br>';
     Conteur ++;
 }
 
 
 function check(element){
 
-	var id=element.id;
-	if (document.getElementById(element.id).checked) {
-		document.getElementById('A'+id).style='text-decoration: line-through';
+	var test =element.id;
+	console.log(document.getElementById(test).checked)
+	if (document.getElementById(test).checked) {
+		document.getElementById(test).checked = true;
+		document.getElementById('A'+test).style='text-decoration: line-through';
 	} else{
-	    document.getElementById('A'+id).style='none';
+	    document.getElementById('A'+test).style='none';
 	}
 }
+
